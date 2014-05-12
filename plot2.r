d2869e49c82138564d9e56af0c3e5512e34fe905
mastortosa <modestomastortosa@gmail.com>
2014-05-12 18:57:42 +0100

    ex2

--- plot2.r
+++ plot2.r
@@ -2,3 +2,6 @@ df <- read.table('household_power_consumption.txt', sep=';', header=T)
 df <- df[df$Date %in% c('1/2/2007', '2/2/2007'),]
 df$Global_active_power <- as.numeric(df$Global_active_power)
 df$Time <- strptime(df$Time, '%H:%M:%S')
+plot(df$Global_active_power / 1000,
+     type='l',
+     ylab='Global active power (kilowatt)')
